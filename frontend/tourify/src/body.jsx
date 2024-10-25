f3a378acf71fa9360e47238cd2873d3d1f9df028
Orwel <jdvanegas@hotmail.com>
2024-10-24 19:57:11 -0500

    Estructura basicaheader, body footer y landing page, detalle producto y panel admin

---
+++ frontend/tourify/src/body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div>
+            <h1>Welcome to Tourify</h1>
+            <p>This is the body component.</p>
+        </div>
+    );
+};
+
+export default Body;
--- frontend/tourify/src/main.jsx
+++ frontend/tourify/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Header from './header'
+import Body from './body'
+import Footer from './footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <Header/>
+    <Body/>
+    <Footer/>
+    
   </StrictMode>,
 )
