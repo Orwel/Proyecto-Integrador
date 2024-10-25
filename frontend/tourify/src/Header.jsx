fdb114a1c4c736500517442b5f81dee4e94942f9
fabianayzaguer <lucasayzaguer@gmail.com>
2024-10-25 02:10:31 -0300

    agregue formato 480 y 768

--- frontend/tourify/src/Header.jsx
+++ frontend/tourify/src/Header.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
+// Header.jsx
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from './assets/images/Logo-fondo-transparente-3.png'; 
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
-        <div className='logo-and-phrase'>
-          <Link to="/" className="logo">
-            <img src={logo} className="logo-image" />
-          </Link>
-          <Link to="/" className="logo">
-            <a>¡Crea recuerdos duraderos... Descubre nuevos horizontes!</a>
-          </Link>
-        </div>
+      <div className='logo-and-phrase'>
+        <Link to="/" className="logo">
+          <img src={logo} className="logo-image" alt="Logo" />
+        </Link>
+        <Link to="/" className="logo">
+          <span>¡Crea recuerdos duraderos... Descubre nuevos horizontes!</span>
+        </Link>
+      </div>
+      
       <div className="auth-buttons">
         <button className="btn" id='btn-crear-cuenta'>Crear cuenta</button>
         <button className="btn" id='btn-iniciar-sesion'>Iniciar sesión</button>
       </div>
-      
-  <div class="hamburger-menu" id="hamburger-menu">
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
+
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {isMenuOpen && (
+        <div className="mobile-menu">
+          <button className="btn" id='btn-crear-cuenta'>Crear cuenta</button>
+          <button className="btn" id='btn-iniciar-sesion'>Iniciar sesión</button>
+        </div>
+      )}
     </header>
   );
 };
