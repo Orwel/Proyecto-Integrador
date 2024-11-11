950fd0c9c3cf0ddb0599ef91d617a125fc9bcda7
gianelitics <20162109@unica.edu.pe>
2024-11-11 17:16:38 -0500

    Crear seccion categorias

--- frontend/tourify/src/utils/cardCategoria.js
+++ frontend/tourify/src/utils/cardCategoria.js
@@ -5,6 +5,7 @@ import cardCategoria3 from "@imagenes/categoria3.png";
 export let cardCategoria = [
 	{
 		id: 1,
+		tipoCategoria: "Aventura y deportes",
 		img1: cardCategoria1,
 		eslogan: "Vuela y siente la adrenalina del parapente en Perú.",
 		tiempo: "3 días, 2 noches",
@@ -13,6 +14,7 @@ export let cardCategoria = [
 	},
 	{
 		id: 2,
+		tipoCategoria: "Cultura y patrimonio",
 		img1: cardCategoria2,
 		eslogan: "Descubre Montevideo sobre ruedas.",
 		tiempo: "3 días, 2 noches",
@@ -21,7 +23,17 @@ export let cardCategoria = [
 	},
 	{
 		id: 3,
+		tipoCategoria: "Tours urbanos",
 		img1: cardCategoria3,
+		eslogan: "Vive la emoción del rafting en Colombia.",
+		tiempo: "3 días, 2 noches",
+		precio: "$ 348/ persona",
+		ubicacion: "Santander, Colombia",
+	},
+	{
+		id: 4,
+		tipoCategoria: "Naturaleza y ecoturismo",
+		img1: cardCategoria2,
 		eslogan: "Vive la emoción del rafting en Colombia.",
 		tiempo: "3 días, 2 noches",
 		precio: "$ 348/ persona",
