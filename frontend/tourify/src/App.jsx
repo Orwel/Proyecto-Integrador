160e303717f7716bf3056e00e94441132609ae79
Juan Carlos <juancarlosvigar@gmail.com>
2024-11-12 17:05:25 -0500

    se agrega la ruta protegida para /panel solo rol id 2

--- frontend/tourify/src/App.jsx
+++ frontend/tourify/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./header";
-import SignUp from './SignUp';
 import './App.css';
 import Body from "./body";
 import Footer from "./footer";
@@ -10,6 +9,7 @@ import { routes } from "./utils/router";
 import Panel from "./Panel";
 import { AdministrarProducto } from "./components/AdministrarProducto";
 import { ActualizarProducto } from "./components/ActualizarProducto";
+import { ProtectedRoute } from "./utils/ProtectedRoute";
 
 function App() {
   return (
@@ -18,8 +18,12 @@ function App() {
       <Routes>
         <Route path={routes.home} element={<Body />} />
         <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/panel" element={<Panel />} />
+        <Route path="/panel" element={
+          <ProtectedRoute>
+            <Panel />
+          </ProtectedRoute>
+        } />
+
         <Route path="/adminProductos" element={<AdministrarProducto />} />
         <Route path="/updateProductos/:id" element={<ActualizarProducto />} />
       </Routes>
