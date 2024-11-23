d38e53d4fb843f6625025b85e7704d5ce58c5de5
Orwel <jdvanegas@hotmail.com>
2024-11-23 15:24:09 -0500

    Corregido error de caracteristicas en detail y actualizar producto

---
+++ frontend/tourify/src/components/ProductFavs.jsx
@@ -0,0 +1,51 @@
+import { useFavorites } from "../hook/use-favorites";
+import { useAuth } from "../context/AuthContext";
+import Heart from "../assets/Icons/Heart";
+import HeartFull from "../assets/Icons/HeartFull";
+import { ModalConfirmation } from "./ModalConfirmation";
+
+const ProductFavs = ({ productId }) => {
+  const { user } = useAuth();
+  const { handleCreate, handleDelete, favorites, modalOpen, modalType, setModalOpen, setModalType } = useFavorites();
+  const isFavorited = favorites.some((fav) => fav.product_id === productId);
+
+
+  const handleFavoriteClick = async () => {
+    if (!user) {
+      // alert("Necesitas iniciar sesión para añadir productos a favoritos.");
+      setModalType("iniciarSesion");
+      setModalOpen(true);
+      return;
+    }
+
+    try {
+      if (isFavorited) {
+        await handleDelete(productId);
+      } else {
+        await handleCreate(productId);
+      }
+    } catch (error) {
+      console.error("Error al manejar favoritos:", error);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={handleFavoriteClick}>
+        {isFavorited ? (<HeartFull />) : (<Heart />)}
+      </button>
+    
+      {/* Mostrar el modal en base al estado */}
+      <ModalConfirmation
+        isOpen={modalOpen}
+        onOpenChange={setModalOpen}
+        type={modalType}
+        onConfirm={() => setModalOpen(false)}
+        navigate={() => {}}
+        role_id={null}
+      />
+    </>
+  );
+};
+
+export default ProductFavs;
