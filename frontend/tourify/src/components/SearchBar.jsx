7e7133bc2f7634e784938e9970185c78d9da0639
Leonardo Sanabria Hoyos <leonardosanh@gmail.com>
2024-11-25 16:10:22 -0500

    Working in search products

--- frontend/tourify/src/components/SearchBar.jsx
+++ frontend/tourify/src/components/SearchBar.jsx
@@ -1,32 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export const SearchBar = () => {
+export const SearchBar = ({ onBuscar }) => {
+	const [startDate, setStartDate] = useState('');
+  	const [endDate, setEndDate] = useState('');
+  	const [filterText, setFilterText] = useState('');
+
+	  const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!startDate || !endDate) {
+		  alert('Por favor, selecciona ambas fechas');
+		  return;
+		}
+		onBuscar({ startDate, endDate, filterText });
+	  };
+
 	return (
 		<>
 			<div className="search-bar">
 				<h2>¿A dónde quieres ir?</h2>
 				<div className="search-fields">
+				<form onSubmit={handleSubmit} className='search-form'>
 					<div className="search-field">
-						<label>Ubicación</label>
-						<input className="search-field" type="search" placeholder="Explora destinos" />
-						
+						<label>
+						Fecha Inicio:
+						<input
+							type="date"
+							value={startDate}
+							onChange={(e) => setStartDate(e.target.value)}
+							className="search-field"
+							placeholder='Agrega fecha de inicio'
+						/>
+						</label>
 					</div>
 					<div className="divider" />
 					<div className="search-field">
-						<label>Check in</label>
-						<input className="search-field" type="search" placeholder="Agrega fecha" />
-						
+						<label>
+						Fecha Fin:
+						<input
+							type="date"
+							value={endDate}
+							onChange={(e) => setEndDate(e.target.value)}
+							className="search-field"
+							placeholder='Agrega fecha de fin'
+						/>
+						</label>
 					</div>
 					<div className="divider" />
 					<div className="search-field">
-						<label>Check out</label>
-						<input className="search-field" type="search" placeholder="Agrega fecha" />
-						
+						<label>
+						Ubicación
+						<input
+							type="text"
+							value={filterText}
+							placeholder="Explora destinos"
+							onChange={(e) => setFilterText(e.target.value)}
+							className="search-field"
+						/>
+						</label>
 					</div>
 					<div className="divider" />
-					<button className="search-button">Buscar</button>
+					<button type="submit" className="search-button">Buscar</button>
+					</form>
 				</div>
 			</div>
 		</>
-	)
-}
+	);
+};
