a279e23a680e08aec5412d4e4631d8546c5fec86
Orwel <jdvanegas@hotmail.com>
2024-12-10 10:15:30 -0500

    Arreglado error sharepopup

--- frontend/tourify/src/components/SharePopup.jsx
+++ frontend/tourify/src/components/SharePopup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import "../styles/share-popup.css";
+import "../styles/SharePopup.css"
 import {
   FaLink,
   FaEnvelope,
