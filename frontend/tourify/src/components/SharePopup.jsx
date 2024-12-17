ea255818af563748594fc10941e38e9704e16bbc
yolandacg <yolanda.castrog@outlook.com>
2024-12-16 21:50:58 -0500

    Ajustando bugs

--- frontend/tourify/src/components/SharePopup.jsx
+++ frontend/tourify/src/components/SharePopup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import "../styles/SharePopup.css"
+import "../styles/Share-popup.css"
 import {
   FaLink,
   FaEnvelope,
